7ef9f64e6ef68e03e24dee2edee6efbe2384b8ee
Thomas Raout <thomraout@gmail.com>
2020-06-03 23:08:24 +0200

    testing in progress

--- handlers/user/handler.user.go
+++ handlers/user/handler.user.go
@@ -18,7 +18,7 @@ func Create(c *gin.Context) {
 
 	// Check formatting
 	if err != nil {
-		c.JSON(400, gin.H{"Incorrect input data": err.Error()})
+		c.JSON(400, gin.H{"Incorrect ay input data": err.Error()})
 		return
 	}
 
--- tests/handler.user_test.go
+++ tests/handler.user_test.go
@@ -1,21 +1,38 @@
 package test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"rakoon/rakoon-back/routes"
+	"strings"
 	"testing"
 
 	"gopkg.in/go-playground/assert.v1"
 )
 
-func UserHandlerTest(t *testing.T) {
+func TestConnect(t *testing.T) {
 	router := routes.SetupRouter()
 
+	body := url.Values{}
+	body.Add("name", "tom")
+	body.Add("password", "qwerty")
+	data := body.Encode()
+
+	req, err := http.NewRequest("POST", "/v1/user", strings.NewReader(data))
+	req.Header.Add("Content-Type", "application/json")
+
+	if err != nil {
+		t.Fail()
+		return
+	}
+
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ping", nil)
 	router.ServeHTTP(w, req)
 
+	fmt.Println(w.Code)
+	fmt.Println(w.Body)
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
 }
